16e93cc45b3ad4f8e8d83caf9ea59f1bdbf5ac18
AbhigyaShridhar <55044948+AbhigyaShridhar@users.noreply.github.com>
2021-07-07 00:38:30 +0530

    Update models.py

--- network/models.py
+++ network/models.py
@@ -62,7 +62,7 @@ class Person(AbstractBaseUser):
     date_joined = models.DateTimeField(verbose_name='date_joined', auto_now_add=True, null=True, blank=True)
     last_login = models.DateTimeField(verbose_name='last_login', auto_now_add=True, null=True, blank=True)
     is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_current = models.BooleanField(default=True)
